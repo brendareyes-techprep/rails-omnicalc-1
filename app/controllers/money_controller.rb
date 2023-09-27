8f89f22c266aafcdda539049178fdb8e633ee1f8
Brenda Reyes <129559415+brendareyes-techprep@users.noreply.github.com>
2023-09-27 15:05:39 +0000

    put in all the work into controller

--- app/controllers/money_controller.rb
+++ app/controllers/money_controller.rb
@@ -6,21 +6,46 @@ class MoneyController < ApplicationController
     render({:template => "square_new"})
   end
   def cucumber
+    @the_num = params.fetch("number").to_f
+
+    @the_result = @the_num ** 2
     render({:template => "square_results"})
   end
+
   def dragonfruit
     render({:template => "square_root_new"})
   end
+  
   def fig
+    @the_num = params.fetch("number").to_f
+
+    @the_result = Math.sqrt(@the_num)
     render({:template => "square_root_results"})
   end
   def grape
-    render({:template => ""})
+    render({:template => "payment_new"})
   end
+  
   def honeydew 
-    render({:template => ""})
+    @the_apr = params.fetch("user_apr").to_f
+    @the_years = params.fetch("user_years").to_f
+    @the_pv = params.fetch("user_pv").to_f
+    r = @the_apr / 100
+    n = @the_years * 12
+    denominator = 1 - (1 + (r / 12) ) ** -n
+    @the_result = ((r / 12 ) * @the_pv) / denominator
+    render({:template => "payment_results"})
   end
+  
   def igo
-    render({:template => ""})
+    render({:template => "random_new"})
+  end
+  
+  def igo
+    @the_min = params.fetch("user_min").to_f
+    @the_max = params.fetch("user_max").to_f
+    @the_result = rand(@the_min..@the_max)
+  
+    render({:template => "random_results"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,11 @@
 Rails.application.routes.draw do
   get("/", {:controller => "money", :action => "apple"})
-  end
   get("/square/new",  {:controller => "money", :action => "banana"})
-  end
   get("/square/results",  {:controller => "money", :action => "cucumber"})
-  end
   get("/square_root/new", {:controller => "money", :action => "dragonfruit"})
-  end
   get("/sqaure_root/results", {:controller => "money", :action => "fig"})
-  end
-  get("/payment", {:controller => "money", :action => ""})
-  end
-  get("/payment/results", {:controller => "money", :action => "grape"})
-  end
-  get("/random/new", {:controller => "money", :action => "honeydew"})
-  end
-  get("/random/results", {:controller => "money", :action => "igo"})
-  end 
+  get("/payment", {:controller => "money", :action => "grape"})
+  get("/payment/results", {:controller => "money", :action => "honeydew"})
+  get("/random/new", {:controller => "money", :action => "igo"})
+  get("/random/results", {:controller => "money", :action => "jasmine"})
 end
